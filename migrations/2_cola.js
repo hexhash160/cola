a86d5e4da0ff6c0f91830dc24a00b7d7aef5cb6e
hash160 <hexhash160@protonmail.com>
2020-09-10 10:24:22 +0800

    add migrations

--- migrations/2_cola.js
+++ migrations/2_cola.js
@@ -1,18 +1,18 @@
-const Cola = artifacts.require("ColaToken")
+const Cola = artifacts.require("Cola")
 
 
-let ownerAddr = '0xee71C9C50eF8D692c8EE553A1ba130e43eDF3a17';
+let ownerAddr = '0x7d3221a00Dd94d5D2EAaC7e71234DC78d4C2A664';
 
 module.exports = async function (deployer, network, accounts) {
     console.log('owner:', accounts[0]);
     ownerAddr = accounts[0];
-    if (network === "ropsten" || network === "mainnet") {
-        //return;
-    } else { // test
-         colaAmount = 100000000;
-         colaDec = 2;
-    }
-    await deployer.deploy(Cola, "Cola", "Cola", BigInt(colaAmount).toString(), colaDec);
-    console.log('Ones:', Cola.address);
+    // if (network === "ropsten" || network === "mainnet") {
+    //     //return;
+    // } else { // test
+    //      colaAmount = 100000000;
+    //      colaDec = 2;
+    // }
+    // await deployer.deploy(Cola, "Cola", "Cola", BigInt(colaAmount).toString(), colaDec);
+    // console.log('Ones:', Cola.address);
 
 };
